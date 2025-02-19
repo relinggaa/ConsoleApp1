def60c58db8614b1805df1eddc773d746acaacdc
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-02-19 07:01:10 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Online C# Editor for free
+// Write, Edit and Run your C# code using C# Online Compiler
+
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        string name = "johns";
+        int a = 1, b = 2;
+        Console.WriteLine(a + b);
+        Console.WriteLine(name);
+        if (name == "johnss")
+        {
+            Console.WriteLine("YA");
+        }
+        else
+        {
+            Console.WriteLine("tidak");
+
+        }
+
+    }
+}
